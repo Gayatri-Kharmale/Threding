04a7313746c0f00ba2e650ba19ec86f6725fbf9c
Gayatri-Kharmale <gayatrikharmale123@gmail.com>
2022-05-15 13:02:09 +0530

    Add project files.

---
+++ Threding/Threading.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threding
+{
+    public class Threading
+    {
+        public void Run()
+        {
+
+
+            Thread t = Thread.CurrentThread;
+            for (int i = 1; i <= 10; i++ )
+            {
+                Console.WriteLine(t.Name);
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+        public void Run2()
+        {
+            Thread t= Thread.CurrentThread;
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(t.Name);
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Threading myThread = new Threading();
+            Threading myThread2 = new Threading();
+            Thread first  = new Thread(new ThreadStart(myThread.Run));
+            Thread second = new Thread(new ThreadStart(myThread.Run));
+            Thread third = new Thread(new ThreadStart(myThread.Run2));
+            first.Name = "First Thread";
+            second.Name = "Second Thread";
+            first.Start();
+            first.Join();
+            second.Start();
+
+
+        }
+    }
+}
